4b8f794028d09e15a398c70469efe3dac21ff3a9
ifc <ifc@campus.concordia.ifc.edu.br>
2025-07-11 08:53:34 -0300

    Banco de Dados

---
+++ Post-Vitam-main/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\ifc.CAMPUS\\Desktop\\Post-Vitam-main\\Post-Vitam-main" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\ifc.CAMPUS\\Desktop\\Post-Vitam-main\\Post-Vitam-main"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\ifc.CAMPUS\\Desktop\\Post-Vitam-main\\Post-Vitam-main\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\ifc.CAMPUS\\Desktop\\Post-Vitam-main\\Post-Vitam-main"
+  "FLUTTER_TARGET=C:\\Users\\ifc.CAMPUS\\Desktop\\Post-Vitam-main\\Post-Vitam-main\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzIuNQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZmNmMmMxMTU3Mg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZGQ5M2RlNmZiMQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\ifc.CAMPUS\\Desktop\\Post-Vitam-main\\Post-Vitam-main\\.dart_tool\\package_config.json"
+)
